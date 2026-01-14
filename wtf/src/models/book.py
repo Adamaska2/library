352b520540c5fb866b31cecf62e6381840e2d0ca
unknown <scpfond1@gmail.com>
2026-01-15 01:45:34 +0300

    Created venv, install pendulum for date showing. Import and add in models/book.pyadded_date for books.

--- wtf/src/models/book.py
+++ wtf/src/models/book.py
@@ -1,19 +1,19 @@
+import pendulum
+
 class Book:
-	def __init__(self, title: str, author: str, year: int, isbn: str):
-		self.title = title
-		self.author = author
-		self.year = year
-		self.isbn = isbn 
-	def get_info(self) -> str:
-	    return f"{self.title}, автор - {self.author}, год издания - {self.year}"
-		
+    def __init__(self, title: str, author: str, year: int, isbn: str):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.isbn = isbn
+        self.added_date = pendulum.now("Europe/Moscow").to_date_string()
+        
+    def get_info(self) -> str:
+        return f"{self.title}, автор - {self.author}, год издания - {self.year} (добавлена: {self.added_date})"
+    
 class EBook(Book):
-	def __init__(self, title: str, author: str, year, format: str, isbn: str):
-		super().__init__(title,author,year, isbn)
-		self.format = format
-	def get_info(self) -> str:
-		return f"{self.title}, автор - {self.author}, год издания - {self.year}, формат - {self.format}"
-
-
-#ebook1 = EBook("test_title","test_author",1997, "pdf", "978-3-16-148410-0")
-#print(eBook1.get_info())		
+    def __init__(self, title: str, author: str, year, format: str, isbn: str):
+        super().__init__(title,author,year, isbn)
+        self.format = format
+    def get_info(self) -> str:
+        return f"{self.title}, автор - {self.author}, год издания - {self.year}, формат - {self.format} (добавлена: {self.added_date})"
