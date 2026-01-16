a3c666179a0c3da6afba568351f3ce6b68bd6bee
Adamaska2 <scpfond1@gmail.com>
2026-01-16 13:29:10 +0300

    Added logging in user.py

--- wtf/src/models/user.py
+++ wtf/src/models/user.py
@@ -1,30 +1,21 @@
+import logging
+
 class User:
-    # cтатическая переменная для id
     id_counter = 1
-
-    def __init__(self, name: str, gender: str, phone: str, borrow_history: str = ""):
-        self.id_user = User.id_counter  # уникальный ID
+    
+    def __init__(self, name: str, gender: str, phone: str):
+        self.id_user = User.id_counter
         User.id_counter += 1
-
         self.name = name
         self.gender = gender
         self.phone = phone
-        self.borrow_history = borrow_history
-        self.books = []  # список взятых книг
-
-    def get_info(self) -> str:
-        return f"{self.name} (ID: {self.id_user}), пол - {self.gender}, телефон - {self.phone}"
-
+        self.books = []
+        
+        logging.info(f"Создан пользователь: {name}, ID: {self.id_user}")
+    
     def borrow_book(self, book):
         """Взять книгу"""
         self.books.append(book)
         book.borrow()
+        logging.info(f"Пользователь {self.name} взял книгу '{book.title}'")
         return f"Пользователь {self.name} взял книгу '{book.title}'"
-
-    def return_book(self, book):
-        """Вернуть книгу"""
-        if book in self.books:
-            self.books.remove(book)
-            book.return_book()
-            return f"Пользователь {self.name} вернул книгу '{book.title}'"
-        return "Книга не найдена"
