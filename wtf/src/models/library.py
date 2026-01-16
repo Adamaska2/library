74fa676f1f0ed1f14bf7bd0c610524b0d87f5932
Adamaska2 <scpfond1@gmail.com>
2026-01-16 13:39:37 +0300

    Added logging in library.py

--- wtf/src/models/library.py
+++ wtf/src/models/library.py
@@ -1,2 +1,21 @@
+import logging
 from .book import Book
 from .user import User
+
+class Library:
+    def __init__(self):
+        self.__books = {}
+        self.__users = {}
+        logging.info("Библиотека создана")
+    
+    def add_book(self, book: Book):
+        """Добавить книгу"""
+        self.__books[book.isbn] = book
+        logging.info(f"Книга добавлена в библиотеку: {book.title}")
+        return f"Книга '{book.title}' добавлена"
+    
+    def add_user(self, user: User):
+        """Добавить пользователя"""
+        self.__users[user.id_user] = user
+        logging.info(f"Пользователь добавлен: {user.name}")
+        return f"Пользователь '{user.name}' добавлен"
