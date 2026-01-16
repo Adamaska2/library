1142cf3e3873af9d3a14584cc30fc7e354128029
Adamaska2 <scpfond1@gmail.com>
2026-01-16 13:27:57 +0300

    Added logging in book.py

--- book.py
+++ book.py
@@ -1,20 +1,24 @@
+import logging
+
 class Book:
     def __init__(self, title: str, author: str, year: int, isbn: str):
         self.title = title
         self.author = author
         self.year = year
         self.isbn = isbn
-        self.is_borrowed = False  # новый атрибут
-
-    def get_info(self) -> str:
-        return f"{self.title}, автор - {self.author}, год издания - {self.year}"
-
+        self.is_borrowed = False
+        
+        # логируем создание книги
+        logging.info(f"Создана книга: {title}, автор: {author}")
+    
     def borrow(self):
         """Взять книгу"""
         self.is_borrowed = True
+        logging.info(f"Книга '{self.title}' взята")
         return f"Книга '{self.title}' взята"
-
+    
     def return_book(self):
         """Вернуть книгу"""
         self.is_borrowed = False
+        logging.info(f"Книга '{self.title}' возвращена")
         return f"Книга '{self.title}' возвращена"
